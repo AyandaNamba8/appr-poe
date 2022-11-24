aefc5a7cdbf94e1bf9e36279566aafde5186583c
AyandaNamba8 <89146595+AyandaNamba8@users.noreply.github.com>
2022-11-24 18:20:38 +0200

    Newly Updated version of POE

---
+++ Pages/DonationStats.cshtml.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace appr_poe
+{
+	public class DonationStatsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
